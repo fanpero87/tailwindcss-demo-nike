f591caf7bfb235b9e205e4cdbfdea794234038c0
FabioPerez <65570296+fanpero87@users.noreply.github.com>
2023-11-05 03:47:05 +0000

    getting all settings ready

---
+++ src/script.js
@@ -0,0 +1,12 @@
+// To showcase the demo of dark theme. Copy paste :)
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleDark = document.getElementById("toggleDark");
+  toggleDark.addEventListener("click", function () {
+    if (document.documentElement.classList.includes("dark")) {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+    alert("click!");
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        palanquin: ["Paranquin", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#ECEEFF",
+        "coral-red": "#FF6452",
+        "slate-gray": "#6D6D6D",
+        "pale-blue": "#F5F6FF",
+        "white-400": "rgba(255,255,255,0.80)",
+      },
+      boxShadow: {
+        "3xl": "0 10px 40px rgba(0, 0, 0, 0.1)",
+      },
+      backgroundImage: {
+        hero: "url('src/assets/images/collection-background.svg')",
+        card: "url('src/assets/images/thumnail-background.svg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
