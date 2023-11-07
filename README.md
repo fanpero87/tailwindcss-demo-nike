bf6906ea58c9bd66fb6d66d90550ad5ab39f4d63
Fabio Perez <65570296+fanpero87@users.noreply.github.com>
2023-11-06 22:45:59 -0500

    including dark mode

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tailwindcss-demo-nike
+
+-   Followed this [youtube video](https://youtu.be/tS7upsfuxmo?si=rTwFNitj-pCjV4di)
+
+-   Didn't use React, just TailwindCSS
+
+-   Used AlpineJS for DarkMode Toggle button
--- src/script.js
+++ src/script.js
@@ -1,12 +1,15 @@
 // To showcase the demo of dark theme. Copy paste :)
-document.addEventListener("DOMContentLoaded", () => {
-  const toggleDark = document.getElementById("toggleDark");
-  toggleDark.addEventListener("click", function () {
-    if (document.documentElement.classList.includes("dark")) {
-      document.documentElement.classList.remove("dark");
-    } else {
-      document.documentElement.classList.add("dark");
-    }
-    alert("click!");
-  });
+
+document.addEventListener('DOMContentLoaded', () => {
+	const toggleDark = document.getElementById('toggleDark');
+
+	console.log(toggleDark.document.documentElement.classList.includes('dark'));
+
+	// toggleDark.addEventListener('click', function () {
+	// 	if (document.documentElement.classList.includes('dark')) {
+	// 		document.documentElement.classList.remove('dark');
+	// 	} else {
+	// 		document.documentElement.classList.add('dark');
+	// 	}
+	// });
 });
